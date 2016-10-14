3eec2f8e79ae8aa7189ca92f1730c943b4b2c7b5
Kyle Evans <kylelarryevans@gmail.com>
2016-10-13 20:53:29 -0600

    done

--- practice.js
+++ practice.js
@@ -6,20 +6,20 @@
 
 // Rewrite the following line so that inner is scoped to the if statement.
 if ( true ) {
-	var inner = "Inner!";
+	let inner = "Inner!";
 }
 
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
-
+const noChange = 100;
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
-
+const vocab = ["let", "var", "destructuring", "spread", "rest"];
 
 // change "var" to "const" at index 1 of vocab
-
+vocab[1] = "const";
 
 // add "arrow function" to the end of vocab
-
+vocab.push("arrow function");
 
 
 ///////////////////////
@@ -27,11 +27,11 @@ if ( true ) {
 //////////////////////
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
-
+var first = 'Kyle', last = 'Evans';
 
 
 // Declare the variable fullName and set it equal to your full name using template literals
-
+var fullName = `${first} ${last}`;
 
 
 //////////////////////
@@ -42,12 +42,22 @@ if ( true ) {
 // first and last should use object literal shorthand (with the variables created above),
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
-
+var me = {
+	first,
+	last,
+	learnES6() {
+		return 'This is awesome'
+	}  ,
+	lovesDevMountain: true
+}
 
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
-
+var flipped = {
+	[first]: 'first',
+	[last]: 'last'
+}
 
 //////////////////////////////
 /* ASSIGNMENT DESTRUCTURING */
@@ -63,7 +73,7 @@ const destructuring = {
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
 
-
+var {objects, arrays} = destructuring;
 
 
 // do not modify
@@ -72,7 +82,7 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
 
-
+var [notVar, alsoNotVar, ...everythingElse] = vocabAgain;
 
 ////////////
 /* ARRAYS */
@@ -82,9 +92,9 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 const firstThree = [ 1, 2, 3 ];
 const lastFive = [ 6, 7, 8, 9, 10 ];
 
-// using the spread operator, create a new array named oneThroughTen.
+// using the spread operator (...), create a new array named oneThroughTen.
 // You should use the above arrays (and 2 missing digits of your own)
-
+const oneThroughTen = [...firstThree, 4, 5, ...lastFive];
 
 // do not modify
 const jsVersions = [ "es5", "es6", "es2017 onward" ];
@@ -99,14 +109,14 @@ function whatToUse( past, present, future ) {
 // using the spread operator, pass each element of jsVersions as an individual argument to whatToUse
 // and save the return value in a variable named currentJS.
 
-
+var currentJS = whatToUse(...jsVersions)
 
 ///////////////
 /* FUNCTIONS */
 ///////////////
 
 // modify greeter to greet Anonymous if no name is passed.
-function greeter( name ) {
+function greeter( name='Anonymous' ) {
 	return `Hi ${ name }!`
 }
 
@@ -114,33 +124,62 @@ function greeter( name ) {
 // write a function named toPower that takes two arguments, a number and an exponent,
 // and returns number to the power of exponent.
 // if no exponent is passed, the exponent should be two
+function toPower(num, exp=2) {
+	return Math.pow(num, exp);
+}
 
+//or
+//
 
 // using rest parameters, write a function named evenOdd that takes in
 // any number of parameters and returns an object with two properties - even and odd.
 // this function should check each parameter and push it to the appropriate property.
-
+function evenOdd(...nums) {
+	const even = [], odd = [];
+  for (let i = 0; i < nums.length; i++) {
+	if (nums[i] % 2 === 0) {
+		even.push(nums[i]);
+	} else {
+		odd.push(nums[i]);
+	}
+ }
+	return {
+		even,
+		odd
+	}
+}
 
 
 // write a function named multiply that takes in a num parameter and an arbitrary amount of
 // additional numbers. This function should return an array of each additional number
 // multiplied by num.
 
-
+function multiply(num, ...nums) {
+  const newArray = [];
+	for (let i = 0; i < nums.length; i++) {
+		newArray.push(num * nums[i]);
+	}
+	return newArray;
+}
+//here is another way:
+// function multiply(num, ...nums) {
+// 	return nums.map(x => x * num)
+// }
 
 // do not modify
 const bits = [ 2, 4, 8, 16, 32, 64, 128 ];
 
 // using an arrow function and the built in .map method, create a new array
 // named mooresBits. mooresBits should be the bits array doubled.
-
+const mooresBits = bits.map( num => num * 2)
 
 // do not modify
 const that = {
-	bind: "returns a function definition with the specified context"
+	  bind: "returns a function definition with the specified context"
 	, call: "invokes a function with the specified context"
 	, apply: "invokes a function with the specified context, passing an array of arguments individually"
 }
 
 // using an arrow function, add a property named arrow that returns the window object
 // do this without using the window keyword. HINT: The default binding of the this keyword
+that.arrow = (arrow => this);
